7dc3527de79a1698f40fa3bf338d0b3577538852
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-29 21:50:38 +0500

    Add: 3) Menyu: hafta kuni [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/3__Menyu__hafta_kuni.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n == 1:
+    print("Mon")
+else:
+    if n == 2:
+        print("Tue")
+    else:
+        if n == 3:
+            print("Wed")
+        else:
+            print("Other")
