24cb070d25cca62d1033694536470e0ef4165f20
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-29 21:50:36 +0500

    Add: 2) Menyu: rang tanlash [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/2__Menyu__rang_tanlash.py
@@ -0,0 +1,11 @@
+color = input().lower()
+if color == "red":
+    print("Stop")
+else:
+    if color == "green":
+        print("Go")
+    else:
+        if color == "yellow":
+            print("Wait")
+        else:
+        	print("Unknown")
