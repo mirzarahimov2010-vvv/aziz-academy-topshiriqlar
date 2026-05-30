6773fcf5a97deaad71650cf5fff29b2b125a6cb0
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:18:14 +0500

    Add: 8) Bahosi berilgan o‘quvchilar [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/8__Bahosi_berilgan_o_quvchilar.py
@@ -0,0 +1,9 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+x = int(input())
+count = sum(1 for student in students if student['score'] == x)
+print(count)
+
