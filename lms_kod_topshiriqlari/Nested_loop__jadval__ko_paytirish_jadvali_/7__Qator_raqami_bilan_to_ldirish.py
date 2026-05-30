b926d7d524746e8afbb63ae78cfbb9b8928ae106
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:03:24 +0500

    Add: 7) Qator raqami bilan to‘ldirish [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Nested_loop__jadval__ko_paytirish_jadvali_/7__Qator_raqami_bilan_to_ldirish.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if j == m:
+            print(i, end="")
+        else:
+            print(i, end=" ")
+    print()
