3f19e76ffc18eb04a132448af72d6045c9596302
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:06:48 +0500

    Add: 13) Menyu + urinishlar soni [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/13__Menyu___urinishlar_soni.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+c = int(input())
+d, e = map(int, input().split())
+f = int(input())
+g = int(input())
+if a == b == c == d == e == f == g:
+    print("Ok")
+else:
+    print("2")
