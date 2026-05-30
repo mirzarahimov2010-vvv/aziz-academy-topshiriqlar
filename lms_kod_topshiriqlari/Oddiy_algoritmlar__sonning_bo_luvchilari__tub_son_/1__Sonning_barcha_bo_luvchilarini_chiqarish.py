c215d42fb305894d4d4d70fe85aed6fbb3730d6a
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:04:08 +0500

    Add: 1) Sonning barcha bo‘luvchilarini chiqarish [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Oddiy_algoritmlar__sonning_bo_luvchilari__tub_son_/1__Sonning_barcha_bo_luvchilarini_chiqarish.py
@@ -0,0 +1,10 @@
+n = int(input())
+divisors = []
+for i in range(1, int(n**0.5) + 1):
+    if n % 1 == 0: 
+        divisors.append(i)
+        if i != n // i:
+            divisors.append(n // i)
+divisors.sort()
+for d in divisors:
+    print(d)
