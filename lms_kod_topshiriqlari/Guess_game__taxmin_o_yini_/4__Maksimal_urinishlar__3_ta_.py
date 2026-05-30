2d36798b04f52eda397096c19ab13bae0dc8d21f
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:05:16 +0500

    Add: 4) Maksimal urinishlar (3 ta) [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/4__Maksimal_urinishlar__3_ta_.py
@@ -0,0 +1,10 @@
+yashirin_son = 8 
+for urinish in range(3):
+    son = int(input())
+    if son == yashirin_son:
+        print("")
+        break 
+    else:
+        print("")
+else:
+    print("Game Over")
