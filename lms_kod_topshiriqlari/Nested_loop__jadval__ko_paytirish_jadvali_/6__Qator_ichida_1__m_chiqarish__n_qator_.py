d73655ab733c116a8cb9715f41363485ed6a4737
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:03:21 +0500

    Add: 6) Qator ichida 1..m chiqarish (n qator) [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Nested_loop__jadval__ko_paytirish_jadvali_/6__Qator_ichida_1__m_chiqarish__n_qator_.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if j == m:
+            print(j, end="")
+        else:
+            print(j, end=" ")
+    print()
+        
