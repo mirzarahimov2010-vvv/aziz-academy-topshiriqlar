fbb9f4ab7b7ab2addd159df033a777f53d3deef4
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:05:10 +0500

    Add: 2) Urinishlar sonini sanash [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/2__Urinishlar_sonini_sanash.py
@@ -0,0 +1,11 @@
+yashirin_son = 5 
+urinishlar = 0 
+while True:
+    son = int(input())
+    urinishlar += 1 
+    if son == yashirin_son:
+        print("")
+        break 
+    else:
+        print("")
+print(urinishlar)
