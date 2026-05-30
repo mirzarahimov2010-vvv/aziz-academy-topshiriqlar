d319c5b11c41ba8229eb98fe5689fc33910ad2ec
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:05:07 +0500

    Add: 1) Oddiy guess game (1..10) [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/1__Oddiy_guess_game__1__10_.py
@@ -0,0 +1,10 @@
+yashirin_son = 7 
+while True:
+    son = int(input())
+    if son < yashirin_son:
+        print("Low")
+    elif son > yashirin_son:
+        print("High")
+    else:
+        print("Correct")
+        break
