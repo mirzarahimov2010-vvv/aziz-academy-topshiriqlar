95e26d60df2034aff47a6bc7618eb0a6b1ec04db
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-05-30 15:06:32 +0500

    Add: 8) Ikki marta amal [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/8__Ikki_marta_amal.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+c = int(input())
+d, e = map(int, input().split())
+f = int(input())
+g = int(input())
+if a == b == c == d == e == f == g:
+    print("Ok")
+else:
+    print("5")
+    print("1")
+    print("Exit")
