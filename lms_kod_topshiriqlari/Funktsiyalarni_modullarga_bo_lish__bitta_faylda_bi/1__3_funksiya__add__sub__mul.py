bb5377cfbf078d2a934e34924208eed89ea86bf5
mirzarahimov2010-vvv <mirzarahimov2010@gmail.com>
2026-06-10 23:59:14 +0500

    Add: 1) 3 funksiya: add, sub, mul [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/Funktsiyalarni_modullarga_bo_lish__bitta_faylda_bi/1__3_funksiya__add__sub__mul.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b 
+
+def sub(a, b):
+    return a - b 
+
+def mul(a, b):
+    return a * b 
+
+a, b = map(int, input().split())
+
+print(add(a, b))
+print(sub(a, b))
+print(mul(a, b))
