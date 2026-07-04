094d991fa2676c5e19bb156b4a5145b340e00c86
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:33:00 +0500

    Add: Kabisa yilni aniqlash [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Kabisa_yilni_aniqlash.py
@@ -0,0 +1,12 @@
+yil = int(input())
+
+if yil % 4 != 0:
+    print("Kabisa emas")
+else:
+    if yil % 100 != 0:
+        print("Kabisa")
+    else:
+        if yil % 400 == 0:
+            print("Kabisa")
+        else:
+            print("Kabisa emas")
