5dddba03e2c68d4b6f10dcf437b7a1fe881c47f1
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:39:32 +0500

    Add: O'lchov birliklari konvertori [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-O_lchov_birliklari_konvertori.py
@@ -0,0 +1,13 @@
+turi = int(input())
+qiymat = int(input())
+
+if turi == 1:
+    print(qiymat * 1000)
+else:
+    if turi == 2:
+        print(qiymat * 60)
+    else:
+        if turi == 3:
+            print(qiymat * 1000)
+        else:
+            print("Notogri tanlov")
