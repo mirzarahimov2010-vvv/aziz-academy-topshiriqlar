437ecc400d29f644c51020d1b9624f0cabc9d16b
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:04:46 +0500

    Add: Tub son tekshiruvi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_son_tekshiruvi.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+if n <= 1:
+    print("yo'q")
+else:
+    is_prime = True 
+    i = 2 
+    
+    
+    while i * i <= n:
+        if n % i == 0:
+            is_prime = False 
+            
+            break
+        i += 1
+        
+        
+    if is_prime:
+        print("ha")
+    else:
+        print("yo'q")
+        
