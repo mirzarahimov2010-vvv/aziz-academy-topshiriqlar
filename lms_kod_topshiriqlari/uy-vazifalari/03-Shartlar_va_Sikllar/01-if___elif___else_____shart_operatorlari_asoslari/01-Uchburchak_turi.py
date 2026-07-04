b8d68742605b328d82a1ff30a4a437a52b172427
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:22:37 +0500

    Add: Uchburchak turi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a <= 0 or b <= 0 or c <= 0 or a + b <= c or a + c <= b or b + c <= a:
+    print("notogri")
+else:
+    if a == b == c:
+        print("teng tomonli")
+    elif a == b or a == c or b == c:
+        print("teng yonli")
+    else:
+        print("turli tomonli")
