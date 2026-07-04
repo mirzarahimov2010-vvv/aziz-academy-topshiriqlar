5b3b961f9a7cdbe03ff356644f05ed794b02db1f
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:26:48 +0500

    Add: Login va parol tekshiruvi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Login_va_parol_tekshiruvi.py
@@ -0,0 +1,10 @@
+login = input()
+parol = input()
+
+if login == "admin":
+    if parol == "1234":
+        print("Xush kelibsiz")
+    else:
+        print("Parol xato")
+else:
+    print("Login topilmadi")
