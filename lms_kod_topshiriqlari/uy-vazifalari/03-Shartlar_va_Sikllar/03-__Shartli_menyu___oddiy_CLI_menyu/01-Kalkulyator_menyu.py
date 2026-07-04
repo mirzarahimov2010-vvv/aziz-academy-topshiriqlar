e71735569b497ae241bf4a2c41b85ad384f55873
Javlon <mirzarahimov2010@gmail.com>
2026-07-05 00:37:58 +0500

    Add: Kalkulyator menyu [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Kalkulyator_menyu.py
@@ -0,0 +1,17 @@
+amal = int(input())
+a = int(input())
+b = int(input())
+
+if amal == 1:
+    print(a + b)
+else:
+    if amal == 2:
+        print(a - b)
+    else:
+        if amal == 3:
+            print(a * b)
+        else:
+            if amal == 4:
+                print(a // b)
+            else:
+                print("Notogri amal")
