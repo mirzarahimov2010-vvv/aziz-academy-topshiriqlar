7bdfc677cdb961e0c33cb7ce48357713915ab154
Javlon <mirzarahimov2010@gmail.com>
2026-07-09 15:57:28 +0500

    Add: Mukammal son tekshiruvi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Mukammal_son_tekshiruvi.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+yigindi = 0 
+
+for i in range(1, n):
+    if n % i == 0:
+        yigindi += i 
+        
+if yigindi == n:
+    print("MUKAMMAL")
+else:
+    print("MUKAMMAL EMAS")
