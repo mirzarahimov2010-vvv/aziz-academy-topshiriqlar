e51298230758519f10d4ad00b7a9464f4559e8b9
Javlon <mirzarahimov2010@gmail.com>
2026-07-10 12:16:34 +0500

    Add: Belgi bo'yicha bitta amal [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,12 @@
+a = int(input())
+amal = input()
+b = int(input())
+
+if amal == "+":
+    print(a + b)
+elif amal == "-":
+    print(a - b)
+elif amal == "*":
+    print(a * b)
+else:
+    print("Noma'lum amal")
