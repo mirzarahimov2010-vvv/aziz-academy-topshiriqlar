062667d63ef66fcee63335c5bfb12692ea74c940
Javlon <mirzarahimov2010@gmail.com>
2026-07-10 23:24:38 +0500

    Add: Menyuli kalkulyator [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Menyuli_kalkulyator.py
@@ -0,0 +1,23 @@
+while True:
+    amal = int(input())
+    
+    if amal == 0:
+        break
+        
+        
+    a = int(input())
+    b = int(input())
+    
+    if amal == 1:
+        print(a + b)
+    elif amal == 2:
+        print(a - b)
+    elif amal == 3:
+        print(a * b)
+    elif amal == 4:
+        if b == 0:
+            print("Xato")
+        else:
+            print(a // b)
+    else:
+        print("Noma'lum")
