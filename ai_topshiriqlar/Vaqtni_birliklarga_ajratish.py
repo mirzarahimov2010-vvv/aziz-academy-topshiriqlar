58f17bb74b6547ea8ac689c0133a893ba1a56b32
Javlon <mirzarahimov2010@gmail.com>
2026-07-21 20:30:31 +0500

    Add AI task: Vaqtni birliklarga ajratish

---
+++ ai_topshiriqlar/Vaqtni_birliklarga_ajratish.py
@@ -0,0 +1,21 @@
+# Vaqtni birliklarga ajratish
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+T = int(input())
+
+kun = T // (24 * 3600)
+qoldiq = T % (24 * 3600)
+
+soat = qoldiq // 3600 
+qoldiq = qoldiq % 3600 
+
+daqiqa = qoldiq // 60 
+sekund = qoldiq % 60 
+
+print(kun)
+print(soat)
+print(daqiqa)
+print(sekund)
