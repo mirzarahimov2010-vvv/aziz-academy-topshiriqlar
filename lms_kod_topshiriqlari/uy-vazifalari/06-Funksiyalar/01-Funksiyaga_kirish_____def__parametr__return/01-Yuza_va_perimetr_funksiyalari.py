65333093b4ceff266d3a1b1d77c8787196905fe5
Javlon <mirzarahimov2010@gmail.com>
2026-08-18 17:27:03 +0500

    Add: Yuza va perimetr funksiyalari [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/06-Funksiyalar/01-Funksiyaga_kirish_____def__parametr__return/01-Yuza_va_perimetr_funksiyalari.py
@@ -0,0 +1,12 @@
+def yuza(en, boyi):
+    return en * boyi 
+
+
+def perimetr(en, boyi):
+    return 2 * (en + boyi)
+
+en = int(input())
+boyi = int(input())
+
+print(yuza(en, boyi))
+print(perimetr(en, boyi))
