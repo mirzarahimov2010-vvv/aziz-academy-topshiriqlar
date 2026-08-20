2dc64a0eb960d2078b0c12d412321e77acb376fb
Javlon <mirzarahimov2010@gmail.com>
2026-08-20 16:05:42 +0500

    Add: 9) Oraliqdagi tub sonlar soni [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/06-Funksiyalar/12-Mustahkamlash_____funksiyalar_bo_yicha_murakkab_ma/09-9__Oraliqdagi_tub_sonlar_soni.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2:
+        return False 
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False 
+        
+    return True
+
+a, b = map(int, input().split())
+print(sum(1 for x in range(a, b + 1) if is_prime(x)))
