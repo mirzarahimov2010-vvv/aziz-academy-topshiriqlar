96cacc27e3e5aea1b66195f5e91818868053fcf1
Javlon <mirzarahimov2010@gmail.com>
2026-08-24 23:55:31 +0500

    Add: Fibonachchi n-hadi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/06-Funksiyalar/12-Mustahkamlash_____funksiyalar_bo_yicha_murakkab_ma/01-Fibonachchi_n-hadi.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n <= 0:
+        return 0 
+    elif n == 1 or n == 2:
+        return 1
+    
+    a, b = 1, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b 
+    return b 
+
+
+n = int(input())
+print(fib(n))
