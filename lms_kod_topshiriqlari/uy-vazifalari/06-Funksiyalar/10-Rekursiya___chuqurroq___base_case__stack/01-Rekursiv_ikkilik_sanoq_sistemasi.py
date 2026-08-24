c9122c45e3a20d734069fe9de46b8dba94ad64ed
Javlon <mirzarahimov2010@gmail.com>
2026-08-24 23:29:25 +0500

    Add: Rekursiv ikkilik sanoq sistemasi [mirzarahimov2010-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/06-Funksiyalar/10-Rekursiya___chuqurroq___base_case__stack/01-Rekursiv_ikkilik_sanoq_sistemasi.py
@@ -0,0 +1,10 @@
+def ikkilik(n):
+    if n == 0:
+        return '0'
+    elif n == 1:
+        return '1'
+    else:
+        return ikkilik(n // 2) + str(n % 2)
+    
+n = int(input())
+print(ikkilik(n))
